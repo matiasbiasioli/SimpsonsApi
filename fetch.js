7e5f6f703e043dc68473b3bd936a630ab252a5f0
matias <matiasbiasioli@gmail.com>
2024-03-18 13:25:59 -0300

    changes

--- fetch.js
+++ fetch.js
@@ -1,42 +1,29 @@
-let div = document.querySelector('.div')
-let cajaSimpsons = document.querySelector(".section")
-let input = document.querySelector('.input')
-let url = 'https://thesimpsonsquoteapi.glitch.me/quotes?count=6'
-let url2 = 'https://thesimpsonsquoteapi.glitch.me/quotes?character='
+let url = 'https://thesimpsonsquoteapi.glitch.me/quotes?count=100'
+// let url2 = 'https://thesimpsonsquoteapi.glitch.me/quotes?count=20&character='
+const select = document.querySelector('#select');
+const cajaSimpsons = document.querySelector('.section');
 
-// btn.addEventListener('click', () => {
-//     div.classList.add('active')
-//     setTimeout(() => {
-//         div.classList.remove('active')
-//     }, 3000)
-// })
-apiSimpsons()
+// Llamada a la API para obtener los datos
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        // Recorremos los datos y los añadimos al select
+        data.forEach(element => {
+            const option = document.createElement('option');
+            option.textContent = element.character;
+            option.value = JSON.stringify(element); // Guardamos todo el objeto JSON como valor de la opción
+            select.appendChild(option);
+            cajaSimpsons.innerHTML = `<div class= "cardContainer"><h2>${element.character}<h2>
+                                        <img class="img" src=${element.image}>
+                                        <p class= "characterParagraph">${element.quote}</p> </div>`;
+        });
+    })
+    .catch(error => console.error('Error al obtener los datos:', error));
 
-input.addEventListener('keyup', characterFilter)
-function characterFilter() {
-    cajaSimpsons.innerHTML = "";
-    fetch(url2 + input.value)
-        .then(resolve => resolve.json())
-        .then(data => {
-            console.log(data)
-            data.forEach(element => {
-                cajaSimpsons.innerHTML += `<div class= "cardContainer"><h2>${element.character}<h2>
-                <img class="img" src=${element.image}>
-                <p class= "characterParagraph">${element.quote}</p> </div>`
-            });
-        })
-}
-
-function apiSimpsons() {
-    fetch(url)
-        .then(resolve => resolve.json())
-        .then(data => {
-            console.log(data)
-            data.forEach(element => {
-                cajaSimpsons.innerHTML += `<div class= "cardContainer"><h2>${element.character}<h2>
-                <img class="img" src=${element.image}>
-                <p class= "characterParagraph">${element.quote}</p> </div>`
-            });
-        })
-}
-
+// Escuchamos el evento cambio del select
+select.addEventListener('change', function () {
+    const selectedElement = JSON.parse(this.value); // Parseamos el JSON guardado como valor
+    cajaSimpsons.innerHTML = `<div class= "cardContainer"><h2>${selectedElement.character}<h2>
+                     <img class="img" src=${selectedElement.image}>
+               <p class= "characterParagraph">${selectedElement.quote}</p> </div>`;
+});
